c75a0e908d85adace6c381291b23d7db3d7f4dc6
Defake <defake@yandex.ru>
2016-11-08 23:12:47 +0500

    com

--- MemoryManagement/Program.cs
+++ MemoryManagement/Program.cs
@@ -10,61 +10,61 @@ namespace MemoryManagement
 	{
 		static void Main(string[] args)
 		{
-			Tes();
+			//Tes();
 
 			Console.ReadKey();
 		}
 
-		public static void Tes()
-		{
-			var timer = new Timer();
-			using (timer.Start())
-			{
-				LongOperation();
-			}
-			var time1 = (int)(timer.TimeInMiliseconds); // Mathematical error is two last symbols
-			Console.WriteLine(time1);
+		//public static void Tes()
+		//{
+		//	var timer = new Timer();
+		//	using (timer.Start())
+		//	{
+		//		LongOperation();
+		//	}
+		//	var time1 = (int)(timer.TimeInMiliseconds); // Mathematical error is two last symbols
+		//	Console.WriteLine(time1);
 
-			using (timer.Start())
-			{
-				LongOperation();
-			}
-			var time2 = (int)(timer.TimeInMiliseconds);
+		//	using (timer.Start())
+		//	{
+		//		LongOperation();
+		//	}
+		//	var time2 = (int)(timer.TimeInMiliseconds);
 			
-			Console.WriteLine(time2);
+		//	Console.WriteLine(time2);
 
-			using (timer.Start())
-			{
-				LongOperation();
-			}
-			var time3 = (int)(timer.TimeInMiliseconds);
-			Console.WriteLine(time3);
+		//	using (timer.Start())
+		//	{
+		//		LongOperation();
+		//	}
+		//	var time3 = (int)(timer.TimeInMiliseconds);
+		//	Console.WriteLine(time3);
 
-			using (timer.Continue())
-			{
-				LongOperation();
-			}
-			var timeCont1 = (int)(timer.TimeInMiliseconds);
-			Console.WriteLine(timeCont1);
+		//	using (timer.Continue())
+		//	{
+		//		LongOperation();
+		//	}
+		//	var timeCont1 = (int)(timer.TimeInMiliseconds);
+		//	Console.WriteLine(timeCont1);
 
-			using (timer.Continue())
-			{
-				LongOperation();
-			}
-			var timeCont2 = (int)(timer.TimeInMiliseconds);
-			Console.WriteLine(timeCont2);
+		//	using (timer.Continue())
+		//	{
+		//		LongOperation();
+		//	}
+		//	var timeCont2 = (int)(timer.TimeInMiliseconds);
+		//	Console.WriteLine(timeCont2);
 
-			//Math.Abs(time1 - time2) < 100;
+		//	//Math.Abs(time1 - time2) < 100;
 
-			Console.ReadKey();
-		}
+		//	Console.ReadKey();
+		//}
 
-		private static void LongOperation()
-		{
-			for (int i = 0; i < 1000000; i++)
-			{
-				var b = new byte[30000];
-			}
-		}
+		//private static void LongOperation()
+		//{
+		//	for (int i = 0; i < 1000000; i++)
+		//	{
+		//		var b = new byte[30000];
+		//	}
+		//}
 	}
 }
